f8543e225349cea49bd49bf4e87a7a0667615b6a
xternet <58574501+xternet@users.noreply.github.com>
2020-05-06 10:58:44 +0200

    .

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,6 +1,6 @@
 const expectRevert = require('openzeppelin-test-helpers/src/expectRevert')
 
-const PREFIX = 'Returned error: VM Exception while processing transaction: '
+const PREFIX = 'Returned error: VM Exception while processing transaction:  '
 
 const waitForEvent = (_event, _from = 0, _to = 'latest') => 
   new Promise ((resolve,reject) => 
